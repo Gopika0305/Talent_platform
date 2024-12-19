3118ba3ebfc3964e674f33d4f63486f4bc35a3b0
Aditya-NV-06 <adityanv4@gmail.com>
2024-12-19 21:01:17 +0530

    feat: Added the skill model.

---
+++ backend/src/models/skill.model.js
@@ -0,0 +1,17 @@
+import mongoose from mongoose;
+
+const mongo = mongoose.schema;
+
+const SkillSchema = new mongo({ 
+     title:{ 
+        type: String,
+        required:true
+     },
+     ratings:{ 
+        type: Number,
+        default:0
+     }
+},{timestamps:true})
+
+
+module.exports = mongoose.model('Skill',SkillSchema)
