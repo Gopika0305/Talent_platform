c4fe3dd09f192442b7d0e6946c1a3cdef33c281c
Aditya-NV-06 <adityanv4@gmail.com>
2024-12-19 20:45:39 +0530

    feat: Added the UserSchema to the models.

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,11 +1,22 @@
 import mongoose from mongoose;
 
 const mongo = mongoose.Schema;
+
 const UserSchema = new mongo({ 
     username:{ 
        type: String,
+       max:6,
+       required:true,
+       unqiue:true
     },
     password:{ 
-        type: String
+        type: String,
+        min:5,
+        max:8,
+        required:true,
     }
-})
+},{timestamps:true})
+
+module.exports = mongoose.model('User',UserSchema)
+
+
