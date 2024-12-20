dccdd826c17cab69675ab019175a2abd9382ee38
Aditya-NV-06 <adityanv4@gmail.com>
2024-12-20 17:17:28 +0530

    feat: Added JWT  auth middleware

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+const authMiddleware = (req,res,next) => {
+    //Get the token as headers.authorization
+    const token = req.headers.authorization;
+    const words = token.split(" ");
+    const jwtToken = words[1]; 
+    //Verify the token
+    const codeWord = jwt.verify(jwtToken,JWT_SECRET);
+
+    if(codeWord.username){ 
+        req.username = codeWord.username;
+        next()
+    }
+    else{ 
+        res.json({
+             msg: "You are not authenticated"
+        })
+    }
+}
+
+export default authMiddleware;
