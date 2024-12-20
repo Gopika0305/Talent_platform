b91336ffa77444fb7f821f1e83c78761d2aaaafa
Aditya-NV-06 <adityanv4@gmail.com>
2024-12-20 21:54:39 +0530

    feat: Added the refactoring the code and boilerplate code for the controllers.

---
+++ backend/src/controllers/user.control.js
@@ -0,0 +1,26 @@
+import { User } from '../exports.js'
+
+const Home = () => { 
+    //logic
+}
+
+const SignIn = (req,res) => { 
+    //logic
+}
+
+const SignUp = (req,res) => { 
+    //logic
+}
+
+const Account = (req,res) => { 
+    //logic
+}
+
+const AccDetails = (req,res) =>{ 
+    //logic
+}
+
+const AccUpdate = (req,res) => { 
+    //logic
+}
+
---
+++ backend/src/exports.js
@@ -0,0 +1,13 @@
+import Experience from '../src/models/experience.model';
+import Skill from '../src/models/skill.model'
+import Register from '../src/models/register.model'
+import User from '../src/models/user.model'
+
+
+
+module.exports = { 
+    Experience,
+    Skill,
+    Register,
+    User
+}
