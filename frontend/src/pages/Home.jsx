2f039f9d34b90e0103f09be40330eea5515a5ad1
Hari Dev <hariharidev5@gmail.com>
2024-12-23 17:49:41 +0530

    chore: Corrected Dropdown toggle issue and added comments in App component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 
+
 const Home = () => {
   return (
     <>
     <h1 className="text-center text-4xl mt-44 font-bold">This is the Home page content</h1>
+
     </>
   )
 }
