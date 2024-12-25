ec32861c4ea551616a5613e76c0759b650613d7c
Aditya-NV-06 <adityanv4@gmail.com>
2024-12-25 18:42:00 +0530

    refactor: Implement lazy loading for route components in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,36 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Login from './pages/Login'
-import Account from './pages/Account'
-import Findtalent from './pages/Findtalent'
-import Jobs from './pages/Jobs'
-import Network from './pages/Network'
-import Notifications from './pages/Notifications'
 import Navbar from './components/Navbar'
 import Main from './pages/Main'
+const Login = React.lazy(() => import('./pages/Login'));
+const Account = React.lazy(() => import('./pages/Account'));
+const Findtalent = React.lazy(() => import('./pages/Findtalent'));
+const Jobs = React.lazy(() => import('./pages/Jobs'));
+const Network = React.lazy(() => import('./pages/Network'));
+const Notifications = React.lazy(() => import('./pages/Notifications'));
 
 
 const App = () => {
   return (
     <>
-  <Router>
-    <div>
-      {/* Navbar Component */}
-    <Navbar/>
+      <Router>
+        <div>
+          {/* Navbar Component */}
+          <Navbar />
 
-    {/* Routing between pages */}
-    
-    <Routes>
-    <Route path="/" element={<Main/>} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/account" element={<Account />} />
-    <Route path="/findtalent" element={<Findtalent/>} />
-    <Route path="/jobs" element={<Jobs/>} />
-    <Route path="/network" element={<Network/>} />
-    <Route path="/Notifications" element={<Notifications/>} />
-
-    </Routes>
-    </div>
-  </Router>
-  </>
+          {/* Routing between pages */}
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Suspense fallback={<div>Loading...</div>}><Login /></Suspense>} />
+            <Route path="/account" element={<Suspense fallback={<div>Loading...</div>}><Account /></Suspense>} />
+            <Route path="/findtalent" element={<Suspense fallback={<div>Loading...</div>}><Findtalent /></Suspense>} />
+            <Route path="/jobs" element={<Suspense fallback={<div>Loading...</div>}><Jobs /></Suspense>} />
+            <Route path="/network" element={<Suspense fallback={<div>Loading...</div>}><Network /></Suspense>} />
+            <Route path="/notifications" element={<Suspense fallback={<div>Loading...</div>}><Notifications /></Suspense>} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   )
 }
 
