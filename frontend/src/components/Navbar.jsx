48deec84aad2bf5b38d1369c24ae57aca1263496
Hari Dev <hariharidev5@gmail.com>
2024-12-26 11:45:17 +0530

    feat:Integrated Google fonts CDN

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,9 +15,9 @@ const Navbar =  () =>{
     </ul>
   </nav> */}
 
-<nav class="fixed top-0 left-0 right-0 bg-white shadow-md z-50">
+<nav class="fixed top-0 left-0 right-0 bg-white shadow-md aleo-regular z-50">
         <div class="container mx-auto px-6 py-3 flex justify-between items-center">
-            <Link to="/" className="text-4xl text-gray-700 hover:text-indigo-600">TalentMatch</Link>
+            <Link to="/" className="text-4xl text-black font-semibold hover:text-indigo-600">TALENTMATCH</Link>
 
             <div className="flex items-center space-x-6">
                 <Link to="/" className="text-gray-700 hover:text-indigo-600">Home</Link>
