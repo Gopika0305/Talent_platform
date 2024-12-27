b9d68ddcc544cd7e536c3c6ddae833dbc9131156
Hari Dev <hariharidev5@gmail.com>
2024-12-27 22:06:58 +0530

    refactor: Aligned the Jobs section and Created a Side bar from MUI

---
+++ frontend/src/components/Jobskeleton.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Jobskeleton= ({ content,position,title,organization}) => {
+  return(
+    <div>
+        
+      <div role="status" className="p-4 mb-4 ml-10 mr-5 border border-gray-200 rounded shadow md:p-6 dark:border-gray-700">
+
+          <div className="flex font-bold w-auto flex-col">
+                <div className="h-2.5 aleo-semibold text-2xl p-2 w-72 mb-8">
+                  {position}
+                </div>
+                <div className="h-auto aleo-regular mb-2.5 mx-4">
+                  {content}
+                </div>
+                <div className="flex flex-row-reverse w-auto mx-4 mt-3">
+                  <svg className="w-10 h-10 me-3 text-gray-200 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                      <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                  </svg>
+                  <div className=" bg-gray-200 font-bold  mt-2 rounded-full dark:bg-gray-700 px-2 mb-2">
+                        {organization}
+                    </div>
+                {/* <div>
+                        <div className=" bg-gray-200 font-bold rounded-full dark:bg-gray-700 px-2 w- mb-2">
+                          {name}
+                        </div>
+                        <div className="w-48 h-6 px-2 font-bold bg-gray-200 rounded-full dark:bg-gray-700">
+                          {position}
+                        </div>
+                    </div> */}
+              </div>
+          </div>
+      </div>
+      </div>
+  )
+};
+
+export default Jobskeleton;
--- frontend/src/pages/Jobs.jsx
+++ frontend/src/pages/Jobs.jsx
@@ -1,18 +1,23 @@
 import { useState,useRef,useEffect } from 'react'
-import Post from '../components/Post'
+import Jobskeleton from '../components/Jobskeleton';
 
 
 const postdata = [
-  { title: "Hiring Designers", content: "This is the first post.", name: "FunWorld", position: "designer" },
-  { title: "Hiring Designers", content: "Yet another post!", name: "FunWorld", position: "designer" },
-  { title: "Hiring Designers", content: "And one more for good measure.", name: "FunWorld", position: "designer" },
-  { title: "Hiring Designers", content: "This is the first post.", name: "FunWorld", position: "designer" }
+  { title: "Hiring Designers", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione quam quas, enim asperiores esse est impedit nihil nulla neque, magnam libero debitis eligendi corrupti porro ab officiis similique dolores sapiente!", position: "Web Developers",organization:"Boring company"},
+  { title: "Hiring Designers", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione quam quas, enim asperiores esse est impedit nihil nulla neque, magnam libero debitis eligendi corrupti porro ab officiis similique dolores sapiente!", position: "Data Analyst",organization:"Boring company" },
+  { title: "Hiring Designers", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione quam quas, enim asperiores esse est impedit nihil nulla neque, magnam libero debitis eligendi corrupti porro ab officiis similique dolores sapiente!", position: "Python Developer",organization:"Boring company" },
+  { title: "Hiring Designers", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione quam quas, enim asperiores esse est impedit nihil nulla neque, magnam libero debitis eligendi corrupti porro ab officiis similique dolores sapiente!", position: "Content Writer",organization:"Boring company" },
 ];
 
 
 const Jobs = () => {
   const [clicked, setClicked] = useState(false)
   const postref = useRef()
+
+const handleJobClick = () => {
+  setClicked(true);
+  
+}
 
   useEffect(() => {
     if (clicked) {
@@ -21,9 +26,14 @@ const Jobs = () => {
   }, [clicked])
   return (
     <>
+    <div>
+
     {postdata.map((post,index)=>{ 
-      return <Post key={index} ref={postref} title={post.title} content={post.content} name={post.name} position={post.position}/>
-    })}
+      return(
+        <button onClick={handleJobClick} >
+       <Jobskeleton key={index} ref={postref} title={post.title} content={post.content} position={post.position} organization={post.organization}/> </button>
+    )})}
+    </div>
        </>
   )
 }
