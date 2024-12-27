cf4ed1ca0633e5e8e934e276e88454f458510e6e
Aditya-NV-06 <adityanv4@gmail.com>
2024-12-27 19:28:25 +0530

    feat: Add Sidebar component and implement smooth scrolling for job posts

--- frontend/src/pages/Jobs.jsx
+++ frontend/src/pages/Jobs.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState,useRef,useEffect } from 'react'
 import Post from '../components/Post'
 
 
@@ -8,11 +8,21 @@ const postdata = [
   { title: "Hiring Designers", content: "And one more for good measure.", name: "FunWorld", position: "designer" },
   { title: "Hiring Designers", content: "This is the first post.", name: "FunWorld", position: "designer" }
 ];
+
+
 const Jobs = () => {
+  const [clicked, setClicked] = useState(false)
+  const postref = useRef()
+
+  useEffect(() => {
+    if (clicked) {
+      postref.current.scrollIntoView({ behavior: "smooth" })
+    }
+  }, [clicked])
   return (
     <>
     {postdata.map((post,index)=>{ 
-      return <Post key={index} title={post.title} content={post.content} name={post.name} position={post.position}/>
+      return <Post key={index} ref={postref} title={post.title} content={post.content} name={post.name} position={post.position}/>
     })}
        </>
   )
