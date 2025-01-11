78e9623e96da91d6e0a549b054eec39e30340c02
Hari Dev <hariharidev5@gmail.com>
2025-01-11 19:58:03 +0530

    Removed wrapper from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,13 +23,13 @@ const App = () => {
 
           {/* Routing between pages */}
           <Routes>
-            <Route path="/" element={<Layout><Main /></Layout>} />
-            <Route path="/login" element={<Suspense ><Layout><Login /></Layout></Suspense>} />
-            <Route path="/register" element={<Suspense><Layout><Register/></Layout></Suspense>}/>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Suspense ><Login /></Suspense>} />
+            <Route path="/register" element={<Suspense><Register/></Suspense>}/>
             <Route path="/findtalent" element={<Suspense ><Findtalent /></Suspense>} />
-            <Route path="/jobs" element={<Suspense ><Layout><Jobs /></Layout></Suspense>} />
-            <Route path="/network" element={<Suspense ><Layout><Network /></Layout></Suspense>} />
-            <Route path="/notifications" element={<Suspense ><Layout><Notifications /></Layout></Suspense>} />
+            <Route path="/jobs" element={<Suspense ><Jobs /></Suspense>} />
+            <Route path="/network" element={<Suspense ><Network /></Suspense>} />
+            <Route path="/notifications" element={<Suspense ><Notifications /></Suspense>} />
           </Routes>
 
           
