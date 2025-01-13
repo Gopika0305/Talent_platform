ca9048bd408bfb9e7bf5db003d8da1a7112d9ed5
Hari Dev <hariharidev5@gmail.com>
2025-01-13 21:31:12 +0530

    refactor:Arranged Notification Section and Added some styling

--- frontend/src/components/NotificationsSkeleton.jsx
+++ frontend/src/components/NotificationsSkeleton.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
 const NotificationsSkeleton = () => {
+    const NotificationData =[
+       {title:"Liked your post", content:"Hello connections..............."},
+       {title:"Commented to your post", content:"Hello connections..............."},
+       {title:"Liked your post", content:"Hello connections..............."},
+       {title:"Posted a job role", content:"Hello connections..............."},
+       {title:"Liked your post", content:"Hello connections..............."},
+       {title:"Liked your post", content:"Hello connections..............."},
+       {title:"Liked your post", content:"Hello connections..............."},
+       {title:"Commented to your post", content:"Hello connections..............."},
+       {title:"Liked your post", content:"Hello connections..............."},
+       {title:"Posted a job role", content:"Hello connections..............."},
+       {title:"Liked your post", content:"Hello connections..............."},
+       {title:"Liked your post", content:"Hello connections..............."},
+       
+       
+    ]
 return (
-    <div role="status" className="max-w-xl ml-40 p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700">
-        {[...Array(5)].map((_, index) => (
+    <>
+    <div role="status" className="max-w-2xl mx-[30%] p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow dark:divide-gray-700 md:p-6 dark:border-gray-700">
+        <span className="text-4xl font-serif ">Notifications</span>
+        {NotificationData.map((_, index) => (
             <div key={index} className="flex items-center justify-between pt-4">
                 <div>
                     <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
@@ -13,6 +31,7 @@ return (
             </div>
         ))}
     </div>
+    </>
 )
 }
 
