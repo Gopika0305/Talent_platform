935e500f28acbc9d726612bb17023a6ce3c29361
Hari Dev <hariharidev5@gmail.com>
2025-01-22 14:35:00 +0530

    Removed unused element from Profile Section

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -6,8 +6,8 @@ const Profile = () => {
         <div className="container mx-auto">
             <h1 className="text-2xl font-bold text-center mt-10">Profile</h1>
             <div className="flex justify-center items-center mt-10">
-            <img src="https://randomuser.me/api/portraits"/>
-            <Animation/>
+            <img src="https://randomuser.me/api/portraits/men/10"/>
+            
             </div>
             </div>
     </>
